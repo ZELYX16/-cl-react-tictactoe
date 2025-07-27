28430c49392cc1219d67bf704adb350c8156de2a
B_Karthikeyan <bkarthy251571@gmail.com>
2025-07-27 18:34:50 +0530

    New build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "-cl-react-tictactoe",
+  base: "/-cl-react-tictactoe/",
 });
