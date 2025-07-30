120a98e81227e1cc293052ae3f85bf04dc777652
B_Karthikeyan <bkarthy251571@gmail.com>
2025-07-30 11:23:26 +0530

    Css changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/-cl-react-tictactoe/',
   plugins: [react()],
-  base: "/-cl-react-tictactoe/",
 });
